bb01f51bc611abf85d7d7629d5d00d2df2154a30
Tamimur <tamimr008@gmail.com>
2023-07-30 01:12:22 +0600

    6 pattern problem from tuf

---
+++ problem_05.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern5(int n);
+int main(){
+    int n;
+    cin>>n;
+    pattern5(n);
+}
+void pattern5(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        }cout<<"\n";
+    }
+}
