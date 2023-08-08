7db43807de475218771b8834c244757467dae0f1
Tamimur <tamimr008@gmail.com>
2023-08-09 02:02:09 +0600

    pattern problem solve 14 from TUF

---
+++ problem_15.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern_13(int n);
+int main()
+{
+    int n;
+    cout << "Input size : ";
+    cin >> n;
+    pattern_13(n);
+}
+void pattern_13(int n)
+{
+    char count = 65 ;
+    for (int i = 1; i <= n; i++)
+    {
+        char count = 65 ;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << count++ << " ";
+        }
+        cout << endl;
+    }
+}
